7395b94969ed474db46d78f15e2b45443a73e252
yashasrnair <yashasrnair@gmail.com>
2025-08-11 02:12:12 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: 'class', // THIS is the key
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
