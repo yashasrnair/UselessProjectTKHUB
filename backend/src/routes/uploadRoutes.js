8e3183c47f171e19d78e9edd4388d922eed3e498
yashasrnair <yashasrnair@gmail.com>
2025-08-10 22:14:10 +0530

    Update uploadRoutes.js

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -1,23 +1,22 @@
+// backend/src/routes/uploadRoutes.js
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const { uploadImage } = require("../controllers/uploadController");
 
 const router = express.Router();
-
 const fs = require("fs");
+
 // Ensure uploads folder exists
 const uploadDir = path.join(__dirname, "../../uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-
 // Store uploads in "uploads" folder
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
-  filename: (req, file, cb) =>
-    cb(null, Date.now() + path.extname(file.originalname)),
+  filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname)),
 });
 const upload = multer({ storage });
 
